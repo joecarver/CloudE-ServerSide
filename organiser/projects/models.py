a5045e3720da743d3ff86ab3eb8a8e776cf455a3
joecarver <joecarver@live.co.uk>
2015-11-02 19:43:56 +0000

    Added unicode representation for Users, Projects

--- organiser/projects/models.py
+++ organiser/projects/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class AppUser(models.Model):
     username = models.CharField(blank=False, max_length=254)
 
+    def __unicode__(self):
+        return self.username
+
 class Project(models.Model):
     title = models.TextField(blank=False)
     admin = models.ForeignKey(AppUser)
+
+    def __unicode__(self):
+        return self.title + ' by: ' + self.admin.username
+    
