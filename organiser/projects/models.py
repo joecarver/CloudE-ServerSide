a480eb83f76195f4b7a4f7f8cfaded09aba878c3
ta2g12 <ta2g12@soton.ac.uk>
2015-11-21 11:19:40 +0000

    merge with VLAD part 1 fix 1

--- organiser/projects/models.py
+++ organiser/projects/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+
 
 # Create your models here.
 
