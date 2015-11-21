3fd4975fa0ebf90df31467c3b0c64ebaf90da254
akavast <vlad.catrici@gmail.com>
2015-11-21 20:13:11 +0000

    GET Users, Users/id, GET Projects Projects/id POST Users, Projects, Column, Task

--- organiser/projects/serializers.py
+++ organiser/projects/serializers.py
@@ -14,23 +14,52 @@ class AppUserSerializer(serializers.ModelSerializer):
  		fields = ('id','username','avatar','projects')
 
 class ProjectSerializer(serializers.ModelSerializer):
-	assignees = serializers.SerializerMethodField('dank')
+	details = serializers.SerializerMethodField('dank')
 	def dank(self, project):
+		
+		projectDetails = {}
+
 		assignees = []
+		columns = []
+		tasks = []
+
 		projAssResults = ProjectAssignee.objects.filter(proj=project.id)
+		projColResults = Column.objects.filter(proj=project.id)
+
 		for projAss in projAssResults:
 			tempResults = AppUser.objects.filter(id=projAss.assignee.id)
-			print tempResults
 			for result in tempResults:
 				tempUser = {}
 				tempUser["id"] = result.id
 				tempUser["username"] = result.username
 				tempUser["avatar"] = result.avatar
 				assignees.append(tempUser)
-		return assignees
+
+		for projCol in projColResults:
+			tempCol = {}
+			tempCol["id"] = projCol.id
+			tempCol["name"] = projCol.name
+			tempCol["proj"] = projCol.proj.id
+			columns.append(tempCol)
+
+			tempTasks =  Task.objects.filter(proj=projCol.proj, column= projCol.id)
+			for task in tempTasks:
+				tempTask = {}
+				tempTask["id"] = task.id
+				tempTask["summary"] = task.summary
+				tempTask["description"] = task.description
+				tempTask["proj"] = task.proj.id
+				tempTask["column"] = task.column.id
+				tasks.append(tempTask)
+
+		projectDetails["assignees"] = assignees
+		projectDetails["columns"] = columns
+		projectDetails["tasks"] = tasks
+
+		return projectDetails
 	class Meta:
 		model = Project
-		fields = ('title', 'admin', 'assignees')
+		fields = ('title', 'admin', 'details')
 
 class TaskSerializer(serializers.ModelSerializer):
 	class Meta:
