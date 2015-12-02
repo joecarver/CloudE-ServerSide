d5133ee5c0dff20356395934e8eaaac98fea5c5f
akavast <vlad.catrici@gmail.com>
2015-12-02 16:23:05 +0000

    perms - changed serializer to see TestObject

--- organiser/projects/serializers.py
+++ organiser/projects/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from projects.models import AppUser, Project, ProjectAssignee, Task, TaskAssignee, TaskRequiredSkill, Notification, Column
+from projects.models import *
 
 #This is the permissions bit
 from django.contrib.auth.models import User
